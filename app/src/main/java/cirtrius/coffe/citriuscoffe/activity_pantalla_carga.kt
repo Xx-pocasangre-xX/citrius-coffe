1ac63cfecb448215afbf12e530191bc9355e4f8a
Ricardo <20230620@ricaldone.edu.sv>
2024-10-03 22:16:21 -0600

    la pantalla de carga ya esta

---
+++ app/src/main/java/cirtrius/coffe/citriuscoffe/activity_pantalla_carga.kt
@@ -0,0 +1,32 @@
+package cirtrius.coffe.citriuscoffe
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class activity_pantalla_carga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pantalla_carga)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(3000)
+
+            val pantallaBienvenida = Intent(this@activity_pantalla_carga, MainActivity::class.java)
+            startActivity(pantallaBienvenida)
+            finish()
+        }
+    }
+}
